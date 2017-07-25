25b75ddb464aa667b0da771c8bc5a3bbc937db1a
Tanaka <herog.vig@gmail.com>
2017-07-26 04:46:19 +0900

    completed.

--- CustomKeyboardExtension/KeyboardViewController.m
+++ CustomKeyboardExtension/KeyboardViewController.m
@@ -61,16 +61,121 @@
 }
     
 -(void) pressInstagramKey {
-    [self.textDocumentProxy insertText:@"a"];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    NSData *data = UIImagePNGRepresentation([UIImage imageNamed:@"instagram.png"]);
+    [pasteboard setData:data forPasteboardType:@"public.png"];
+    
+    if (pasteboard) {
+        
+        //copy image to clipboard
+        UIView *notificationView= [[UIView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height * 0.1, self.view.frame.size.width, 30)];
+        notificationView.backgroundColor = [UIColor greenColor];
+        UILabel *notification = [[UILabel alloc]initWithFrame: CGRectMake(0, 0, notificationView.frame.size.width, notificationView.frame.size.height)];
+        notification.text = @"Instagram icon copied to clipboard!";
+        notification.textColor = [UIColor whiteColor];
+        notification.textAlignment = NSTextAlignmentCenter;
+        [notificationView addSubview:notification];
+        
+        [notificationView setAlpha:0.0f];
+        
+        //fade in
+        [UIView animateWithDuration:1.0f animations:^{
+            
+            [notificationView setAlpha:1.0f];
+            
+        } completion:^(BOOL finished) {
+            
+            //fade out
+            [UIView animateWithDuration:0.5f animations:^{
+                
+                [notificationView setAlpha:0.0f];
+                
+            } completion:nil];
+            
+        }];
+        
+        [self.view addSubview:notificationView];
+
+    }
     
 }
     
 -(void) pressFCBKey {
-    [self.textDocumentProxy insertText:@"b"];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    NSData *data = UIImagePNGRepresentation([UIImage imageNamed:@"fcb.png"]);
+    [pasteboard setData:data forPasteboardType:@"public.png"];
+    
+    if (pasteboard) {
+        
+        //copy image to clipboard
+        UIView *notificationView= [[UIView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height * 0.1, self.view.frame.size.width, 30)];
+        notificationView.backgroundColor = [UIColor greenColor];
+        UILabel *notification = [[UILabel alloc]initWithFrame: CGRectMake(0, 0, notificationView.frame.size.width, notificationView.frame.size.height)];
+        notification.text = @"FCB icon copied to clipboard!";
+        notification.textColor = [UIColor whiteColor];
+        notification.textAlignment = NSTextAlignmentCenter;
+        [notificationView addSubview:notification];
+        
+        [notificationView setAlpha:0.0f];
+        
+        //fade in
+        [UIView animateWithDuration:1.0f animations:^{
+            
+            [notificationView setAlpha:1.0f];
+            
+        } completion:^(BOOL finished) {
+            
+            //fade out
+            [UIView animateWithDuration:0.5f animations:^{
+                
+                [notificationView setAlpha:0.0f];
+                
+            } completion:nil];
+            
+        }];
+        
+        [self.view addSubview:notificationView];
+        
+    }
 }
     
 -(void) pressTwitterKey {
-    [self.textDocumentProxy insertText:@"c"];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    NSData *data = UIImagePNGRepresentation([UIImage imageNamed:@"twitter.png"]);
+    [pasteboard setData:data forPasteboardType:@"public.png"];
+    
+    if (pasteboard) {
+        
+        //copy image to clipboard
+        UIView *notificationView= [[UIView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height * 0.1, self.view.frame.size.width, 30)];
+        notificationView.backgroundColor = [UIColor greenColor];
+        UILabel *notification = [[UILabel alloc]initWithFrame: CGRectMake(0, 0, notificationView.frame.size.width, notificationView.frame.size.height)];
+        notification.text = @"Twitter icon copied to clipboard!";
+        notification.textColor = [UIColor whiteColor];
+        notification.textAlignment = NSTextAlignmentCenter;
+        [notificationView addSubview:notification];
+        
+        [notificationView setAlpha:0.0f];
+        
+        //fade in
+        [UIView animateWithDuration:1.0f animations:^{
+            
+            [notificationView setAlpha:1.0f];
+            
+        } completion:^(BOOL finished) {
+            
+            //fade out
+            [UIView animateWithDuration:0.5f animations:^{
+                
+                [notificationView setAlpha:0.0f];
+                
+            } completion:nil];
+            
+        }];
+        
+        [self.view addSubview:notificationView];
+        
+    }
 }
     
     
